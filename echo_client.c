291c9491a0042b8f511978ce98314cf83bc722af
Nathan Miniovich <nathan.miniovich@rutgers.edu>
2015-05-20 12:03:39 +0900

    preliminary client build

--- echo_client.c
+++ echo_client.c
@@ -19,4 +19,25 @@ int main(int argc, char** argv){
 	memset(&addr,0,sizeof(addr));
 	socketDescriptor = socket(AF_INET,SOCK_STREAM,0);
 
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET,"192.168.100.155",&saddr.sin_addr.s_addr)
+	addr.sin_port = htons(50000);
+
+	connect(socketDescriptor,(struct sockaddr*)&addr, sizeof(addr));
+
+	for(; ;){
+		memset(msg,0,MAXBUFSIZE); //clear message and echo buffers on each iteration
+		memset(echo,0,MAXBUFSIZE);
+
+		fgets(msg,MAXBUFSIZE,stdin);
+		send(socketDescriptor,msg,strlen(msg),0);
+		recv(socketDescriptor,echo,sizeof(echo),0);
+
+		printf("%s",echo);
+		fflush(stdout); //for compatibility with older systems that need to have the stdout buffer dumped
+
+	}
+
+	exit(0);
+
 }
